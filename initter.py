9494a3ffce8e0caede9fd92fdcdd59a23f9d225f
latufla <lafufla.work@gmail.com>
2017-10-22 19:27:47 +0700

    85.143.173.86

--- initter.py
+++ initter.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = "iodehjoiwjetsadfokiapserq"
 
+# simple cloud host 85.143.173.86
 app.config.from_pyfile("app.cfg")
 
 db = SQLAlchemy(app)
